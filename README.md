68059251b5be32ad0ca7162c18c0e81b7ccbfdf6
Kwinten Pisman <kwinten.pisman@gmail.com>
2020-07-13 16:00:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# stencil-standalone-repros
---
+++ issue1/worker.js
@@ -0,0 +1,45 @@
+importScripts('https://cdn.jsdelivr.net/npm/@stencil/core/compiler/stencil.js');
+importScripts('https://cdn.jsdelivr.net/npm/typescript@3.9.6/lib/typescript.js')
+
+console.log('worker inited');
+debugger;
+stencil.transpile(`/* @jsx h */
+import '@stencil/router';
+import { h, Component, State, Host } from "@stencil/core";
+
+@Component({
+  tag: "my-counter",
+  // styleUrl: "index.css",
+  shadow: true,
+})
+export class MyCounter {
+  @State() count: number = 4;
+
+  inc() {
+    this.count++;
+  }
+
+  dec() {
+    this.count--;
+  }
+
+  render() {
+    return (
+      <Host>
+        <button onClick={this.dec.bind(this)}>-</button>
+        <span>{this.count}</span>
+        <button onClick={this.inc.bind(this)}>+</button>
+      </Host>
+    );
+  }
+}`, {
+    file: 'index.tsx',
+    module: 'cjs',
+    // script: 'es5',
+    style: 'import',
+    // componentMetadata: 'compilerstatic'
+}, null).then(results => {
+    const errors = results.diagnostics[0];
+
+
+})
